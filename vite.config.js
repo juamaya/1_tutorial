7cacc6dd6f6fecdfafad3f04e78975179f548914
juamaya <marinamaya@hotmail.es>
2025-07-27 18:43:51 +0200

    Add icono

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),],
-   base: '/1_tutorial/'
-})
+  plugins: [react(), tailwindcss(), visualizer({ open: true })], // abre un reporte interactivo al build],
+  base: "/1_tutorial/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800, // opcional: subir límite si sigue advirtiendo
+  },
+});
